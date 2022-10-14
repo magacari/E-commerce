195dd593dc8d6e23823e966cc17b78fdc18d9b67
marcos castillo <marcos_gabriel_cr@hotmail.com>
2022-10-13 21:43:51 -0300

    agregando nuevo archivo js para librerias

---
+++ js/librerias.js
@@ -0,0 +1,12 @@
+let procesoDesarrollo  = document.getElementById("procesoDesarrollo");
+
+procesoDesarrollo.addEventListener("click", () =>{
+    Swal.fire({
+        title: "Ooops....",
+        text: "Esto todavia no esta desarrollado, pronto vendrán mas actualizaciones",
+        imageUrl: "https://w7.pngwing.com/pngs/579/179/png-transparent-human-working-with-computer-meme-know-your-meme-computer-workplace-characters-angle-white-culture-thumbnail.png",
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: "trabajando en eso"
+    })
+});
